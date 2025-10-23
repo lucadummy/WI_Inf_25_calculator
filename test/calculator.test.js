a81b150a29d604da23ac5550e945ee52d5ea275c
LucaGrab <luca@grabowski.com>
2025-10-23 21:14:54 +0200

    changed test case

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,6 +1,6 @@
 const calc = require('../src/calculator');
 test('add', () => expect(calc.add(1,2)).toBe(3));
-test('sub', () => expect(calc.sub(6,4)).toBe(2));
+test('sub', () => expect(calc.sub(5,4)).toBe(1));
 test('mul', () => expect(calc.mul(2,3)).toBe(6));
 test('div', () => expect(calc.div(6,3)).toBe(2));
 test('div by zero throws', () => expect(() => calc.div(1,0)).toThrow());
